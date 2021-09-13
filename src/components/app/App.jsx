81998f8483fe0e9699088776246a0b7875846a08
Derek Hudgins <hudgins.derek@gmail.com>
2021-09-13 14:00:15 -0700

    set up router

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,12 +1,27 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from '../../containers/Home';
+import History from '../pages/History';
+
 
 const App = () => {
   return (
-    <>
-      <Home />
-    </>
+    <div className="main">
+      <Router>
+        <Switch>
+          <Route
+            path="/"
+            exact
+            render={(routerProps) => <Home {...routerProps} />}
+          />
+          <Route
+            path="/history"
+            exact
+            render={(routerProps) => <History {...routerProps} />}
+          />
+        </Switch>
+      </Router>
+    </div>
   );
 };
-
 export default App;
