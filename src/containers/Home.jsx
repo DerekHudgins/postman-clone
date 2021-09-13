69fd1d1c387fbfe46dbf14e50a02d4ab8c6e9e19
Derek Hudgins <hudgins.derek@gmail.com>
2021-09-13 13:50:54 -0700

    setup Home and Header for CSS

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import UserInterface from '../components/pages/UserInterface';
+import Header from '../components/pages/Header';
+
 
 export default class Home extends Component {
   render() {
     return (
       <>
+        <Header />
         <UserInterface />
       </>
     );
