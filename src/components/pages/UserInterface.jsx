d9886d291e6bf26ba63a9c503d7e46bdcab82b1f
Derek Hudgins <hudgins.derek@gmail.com>
2021-09-13 13:40:33 -0700

    added basic component struct

---
+++ src/components/pages/UserInterface.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+const UserInterface = () => {
+  return <div></div>;
+};
+
+UserInterface.propTypes = {};
+
+export default UserInterface;
