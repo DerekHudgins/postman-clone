776b367b0c258b92efda862691c3463941184f3f
Derek Hudgins <hudgins.derek@gmail.com>
2021-09-14 10:28:13 -0700

    added more styles

--- src/components/pages/UserInterface.jsx
+++ src/components/pages/UserInterface.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './styles/UserInterface.css';
 
-
 const UserInterface = (props) => {
   return (
     <main className={styles.UserInterface}>
@@ -12,18 +11,22 @@ const UserInterface = (props) => {
       </div>
       <div className={styles.radioButtons}>
         <form>
-          <button type="radio" name="get">
+          <label htmlFor="get">
             GET
-          </button>
-          <button type="radio" name="put">
+            <input type="radio" id="get" name="method" />
+          </label>
+          <label htmlFor="put">
             PUT
-          </button>
-          <button type="radio" name="post">
+            <input type="radio" id="put" name="method" />
+          </label>
+          <label htmlFor="post">
             POST
-          </button>
-          <button type="radio" name="delete">
+            <input type="radio" id="post" name="method" />
+          </label>
+          <label htmlFor="delete">
             DELETE
-          </button>
+            <input type="radio" id="delete" name="method" />
+          </label>
         </form>
       </div>
       <div className={styles.JSONInput}>
