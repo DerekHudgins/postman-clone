2a0a3eef5292bd0b7f7f06964723fea53049e153
Derek Hudgins <hudgins.derek@gmail.com>
2021-09-14 10:30:43 -0700

    moved router and turned app back into class

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,27 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Header from '../headers/Header';
 import Home from '../../containers/Home';
 import History from '../pages/History';
 
 
-const App = () => {
-  return (
-    <div className="main">
-      <Router>
-        <Switch>
-          <Route
-            path="/"
-            exact
-            render={(routerProps) => <Home {...routerProps} />}
-          />
-          <Route
-            path="/history"
-            exact
-            render={(routerProps) => <History {...routerProps} />}
-          />
-        </Switch>
-      </Router>
-    </div>
-  );
-};
-export default App;
+export default class App extends Component {
+  render() {
+    return (
+      <div className="main">
+        <Router>
+          <Header />
+          <Switch>
+            <Route
+              path="/"
+              exact
+              render={(routerProps) => <Home {...routerProps} />}
+            />
+            <Route
+              path="/history"
+              exact
+              render={(routerProps) => <History {...routerProps} />}
+            />
+          </Switch>
+        </Router>
+      </div>
+    );
+  }
+}
