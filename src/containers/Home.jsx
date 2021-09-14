caf49a0343fa6d025045656cf87aeaac0ebf827a
Derek Hudgins <hudgins.derek@gmail.com>
2021-09-14 11:33:01 -0700

    pushing current working state

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import UserInterface from '../components/pages/UserInterface';
+import fetchAPI from '../services/fetchAPI';
 import styles from './styles/Home.css';
 
 export default class Home extends Component {
@@ -7,18 +8,43 @@ export default class Home extends Component {
     url: '',
     method: '',
     JSON: '',
-    result: {},
+    response: '',
   };
 
   handleChange = ({ target }) => {
     this.setState({ [target.name]: target.value });
   };
 
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.handleFetch();
+  };
+
+  handleFetch = () => {
+    const { url, method, JSON } = this.state;
+
+    fetchAPI(url, method, JSON).then(
+      (res) => this.setState({ response: res }),
+      () => {
+        console.log(this.state.response);
+      }
+    );
+  };
+
   render() {
-    const { handleChange } = this;
+    const { handleChange, handleSubmit } = this;
+    const { results, method, JSON, response } = this.state;
+    console.log(response);
     return (
       <div className={styles.Home}>
-        <UserInterface handleChange={handleChange} />
+        <UserInterface
+          handleChange={handleChange}
+          handleSubmit={handleSubmit}
+          results={results}
+          method={method}
+          JSON={JSON}
+          response={response}
+        />
       </div>
     );
   }
